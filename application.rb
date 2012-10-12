e8f13c8b45de0b47894058a61e7107f458ac9cfd
Johannes Gorset <jgorset@gmail.com>
2012-10-12 11:34:57 +0200

    Use Sinatra's configure block instead of constants

--- application.rb
+++ application.rb
@@ -2,11 +2,13 @@ require "sinatra"
 require "json"
 require "pony"
 
-# A string describing the odds of a code review (e.g. "1:10").
-ODDS = ENV["ODDS"]
+configure do
+  # A string describing the odds of a code review (e.g. "1:10").
+  set :odds, ENV["ODDS"]
 
-# A list of strings describing e-mail addresses a code review may be addressed to.
-RECIPIENTS = ENV["RECIPIENTS"].split ","
+  # A list of strings describing e-mail addresses a code review may be addressed to.
+  set :recipients, ENV["RECIPIENTS"].split ","
+end
 
 Pony.options = {
   via: :smtp,
@@ -24,12 +26,12 @@ Pony.options = {
 post "/" do
   data = JSON.parse request.body.read
 
-  x, y = ODDS.split ":"
+  x, y = settings.odds.split ":"
   chance = ((x.to_f / (y.to_f - 1)) * 100)
 
   data["commits"].each do |commit|
     if rand(100) <= chance
-      eligible_recipients = RECIPIENTS.reject { |recipient| recipient =~ /#{commit["author"]["email"]}/ }
+      eligible_recipients = settings.recipients.reject { |recipient| recipient =~ /#{commit["author"]["email"]}/ }
 
       raise StandardError, "No eligible recipients" if eligible_recipients.empty?
 
