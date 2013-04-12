dc1752ab262e1224871ed5a7d04a327d6bac9a67
Johannes Gorset <jgorset@gmail.com>
2013-04-12 17:33:13 +0200

    Refactor application test

--- test/application_test.rb
+++ test/application_test.rb
@@ -7,35 +7,35 @@ class ApplicationTest < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_responds
-    Pony.expects(:mail).twice
-
-    post "/", JSON.dump({
-      :before => "95790bf891e76fee5e1747ab589903a6a1f80f22",
-      :after => "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
-      :ref => "refs/heads/master",
-      :user_id => 4,
-      :user_name => "John Smith",
-      :repository => {
-        :name => "Diaspora",
-        :url => "localhost/diaspora",
-        :description => "",
-        :homepage => "localhost/diaspora",
-        :private => true
+  def test_sends_email
+    payload = {
+      before: "95790bf891e76fee5e1747ab589903a6a1f80f22",
+      after: "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
+      ref: "refs/heads/master",
+      user_id: 4,
+      user_name: "John Smith",
+      repository: {
+        name: "Diaspora",
+        url: "localhost/diaspora",
+        description: "",
+        homepage: "localhost/diaspora",
+        private: true
       },
-      :commits => [
+      commits: [
         {
-          :id => "450d0de7532f8b663b9c5cce183b...",
-          :message => "Update Catalan translation to e38cb41.",
-          :timestamp => "2011-12-12T14:27:31+02:00",
-          :author => {
-            :name => "Johannes Gorset",
-            :email => "johannes@hyper.no"
+          id: "450d0de7532f8b663b9c5cce183b...",
+          message: "Update Catalan translation to e38cb41.",
+          timestamp: "2011-12-12T14:27:31+02:00",
+          author: {
+           name: "Johannes Gorset",
+           email: "johannes@hyper.no"
           }
         }
       ]
-    })
+    }
 
-    assert last_response.ok?
+    2.times { Pony.expects :mail }
+
+    post "/", payload.to_json
   end
 end
