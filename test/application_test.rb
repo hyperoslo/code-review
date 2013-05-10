026b3632c617a6a10aae2d39ada0fd4478523d92
Johannes Gorset <jgorset@gmail.com>
2013-05-10 23:25:51 +0200

    Use fixtures

--- test/application_test.rb
+++ test/application_test.rb
@@ -8,33 +8,6 @@ class ApplicationTest < MiniTest::Unit::TestCase
   end
 
   def test_sends_email
-    payload = {
-      before: "95790bf891e76fee5e1747ab589903a6a1f80f22",
-      after: "da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
-      ref: "refs/heads/master",
-      user_id: 4,
-      user_name: "John Smith",
-      repository: {
-        name: "Diaspora",
-        url: "localhost/diaspora",
-        description: "",
-        homepage: "localhost/diaspora",
-        private: true
-      },
-      commits: [
-        {
-          id: "b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
-          message: "Update Catalan translation to e38cb41.",
-          timestamp: "2011-12-12T14:27:31+02:00",
-          url: "http://localhost/diaspora/commits/b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
-          author: {
-           name: "Johannes Gorset",
-           email: "johannes@hyper.no"
-          }
-        }
-      ]
-    }
-
     GitLab.
       expects(
         :diff
@@ -54,12 +27,12 @@ class ApplicationTest < MiniTest::Unit::TestCase
         )
       )
 
-    post "/", payload.to_json
+    post "/", fixture("gitlab.json")
   end
 
   def test_github_webhook
     Pony.stubs(:mail).returns true
     GitHub.stubs(:diff)
-    post "/", payload: github_payload.to_json
+    post "/", payload: fixture("github.json")
   end
 end
