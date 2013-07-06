348ce1b8645b4d1a0bec5162154c3975cbce9b48
Harald Ringvold <harald.ringvold@gmail.com>
2013-07-06 21:03:04 +0200

    Adding groups feature.

--- lib/reviewers.rb
+++ lib/reviewers.rb
@@ -12,24 +12,44 @@ module Reviewers
 
     # Find reviewers eligible to review the given e-mail address.
     #
-    # email - A String describing an e-mail address.
+    # email 	- A String describing an e-mail address.
+    # groups 	- An Array of groups.
     #
     # Returns an Array of Reviewer instances.
-    def for email
-      reject do |reviewer|
-        reviewer.emails.include? email
-      end
+    def for email, groups=nil
+    	if groups
+    		reviewers = select do |reviewer|
+	        reviewer.has_groups? groups
+	      end
+	      reviewers.reject do |reviewer|
+	        reviewer.emails.include? email
+	      end
+    	else
+	      reject do |reviewer|
+	        reviewer.emails.include? email
+	        reviewer.groups.size > 0 				# only include reviwers that does not have groups
+	      end
+    	end
     end
 
     # Load reviewers.
     #
-    # reviewers - A String describing a comma- and colon-separated list of
+    # reviewers - A String describing a comma- , colon- and semicolon-seperated list of
     #             reviewers (see the documentation for details).
     def load reviewers
       @reviewers = reviewers.split(",").map do |reviewer|
         addresses = reviewer.split ":"
+        groups = []
 
-        Reviewer.new addresses.shift, addresses
+        # look for groups
+        addresses.reject! do |address|
+	        if address.include? ";"
+	        	groups = address.split ";"
+	        	addresses << groups.shift    # put last email before groups back into address array
+	        end
+        end
+
+        Reviewer.new addresses.shift, addresses, groups
       end
     end
   end
--- test/reviewers_test.rb
+++ test/reviewers_test.rb
@@ -3,16 +3,17 @@ require "reviewers"
 
 class ReviewersTest < MiniTest::Unit::TestCase
   def setup
-    Reviewers.load "me@work:me@home:me@road,you@work:you@home"
+    Reviewers.load "me@work:me@home:me@road;ruby;javascript,you@work:you@home,him@work:him@home;ruby,her@work:her@home;python"
   end
 
   def test_load
-    assert_equal 2, Reviewers.entries.size
+    assert_equal 4, Reviewers.entries.size
 
     reviewer = Reviewers.entries.first
 
     assert_equal "me@work", reviewer.email
     assert_equal ["me@home", "me@road"], reviewer.aliases
+    assert_equal ["ruby", "javascript"], reviewer.groups
   end
 
   def test_for
@@ -20,5 +21,12 @@ class ReviewersTest < MiniTest::Unit::TestCase
 
     assert_equal 1, reviewers.size
     assert_equal "you@work", reviewers.first.email
+  end
+
+  def test_for_with_groups
+    reviewers = Reviewers.for "me@home", ["ruby", "python"]
+
+    assert_equal 2, reviewers.size
+    assert_equal "him@work", reviewers.first.email
   end
 end
